c254417c9af61d8a4d04ab2a93f202bc31de2858
dbozich1@gmail.com <dbozich1@gmail.com>
2018-04-21 04:06:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Derrick"
-var height = "4"
+var height = 4
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height))
+console.log(height)
